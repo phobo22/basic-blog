85f1867e20d892f97995b4ebd91cefb024120036
22127025 <ptbang22@clc.fitus.edu.vn>
2025-10-08 22:59:48 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./static/css/index.css">
+    <title>PTB Blog</title>
+</head>
+<body>
+    <div class="container">
+        <h1 id="title">Welcome to PTB Blog - Connect you to the world</h1>
+        <div>
+            <a href="./static/login.php" id="login">Login</a>
+            <a href="./static/signup.php" id="signup">Sign Up</a>
+        </div>
+    </div>
+</body>
+</html>
